074e55b5159ac2412985bef050967e2e3cc2c44a
reactdevelopmentlpcc@gmail.com <jeffrey.graham600@gmail.com>
2023-03-24 19:44:05 -0400

    working out drag bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Algos from "./components/Grid/Grid";
+import Grid from "./components/Grid/Grid";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Algos />;
+      <Grid />;
     </div>
   );
 }
--- src/components/Grid/ViewGrid.tsx
+++ src/components/Grid/ViewGrid.tsx
@@ -16,6 +16,15 @@ interface Props {
 
 const ViewGrid: React.FC<Props> = ({ grid }) => {
   const [viewGrid, setViewGrid] = useState(grid);
+  const [currentNode, setCurrentNode] = useState<any>({
+    isWall: false,
+    isStart: false,
+    isFinish: false,
+    row: 4,
+    col: 4,
+  });
+
+  const [mouseClick, setMouseClick] = useState<boolean>(false);
 
   const changeState = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const currentId = e.currentTarget.id;
@@ -26,7 +35,49 @@ const ViewGrid: React.FC<Props> = ({ grid }) => {
     setViewGrid(tempGrid);
   };
 
-  console.log(viewGrid);
+  const mouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const currentId = e.currentTarget.id;
+    let parsedId = currentId.split(",").map((x) => Number(x));
+    let node = viewGrid[parsedId[0]][parsedId[1]];
+
+    if ((node.isStart || node.isFinish) && !node.isWall) {
+      setCurrentNode(node);
+      setMouseClick((prevMouseClick) => (prevMouseClick = true));
+    }
+  };
+
+  const mouseUp = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const currentId = e.currentTarget.id;
+    let parsedId = currentId.split(",").map((x) => Number(x));
+    let node = viewGrid[parsedId[0]][parsedId[1]];
+
+    if ((node.isStart || node.isFinish) && !node.isWall) {
+      setMouseClick((prevMouseClick) => (prevMouseClick = false));
+    }
+  };
+
+  const onMouseEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (mouseClick) {
+      const currentId = e.currentTarget.id;
+      let parsedId = currentId.split(",").map((x) => Number(x));
+      let node = viewGrid[parsedId[0]][parsedId[1]];
+
+      let tempGrid = [...viewGrid];
+      tempGrid[currentNode.row][currentNode.col] = { ...node };
+      tempGrid[parsedId[0]][parsedId[1]] = { ...currentNode };
+
+      setViewGrid(tempGrid);
+    }
+  };
+
+  const onMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (mouseClick) {
+      const currentId = e.currentTarget.id;
+      let parsedId = currentId.split(",").map((x) => Number(x));
+      let node = viewGrid[parsedId[0]][parsedId[1]];
+      setCurrentNode({ ...node, isStart: true });
+    }
+  };
 
   return (
     <>
@@ -36,6 +87,10 @@ const ViewGrid: React.FC<Props> = ({ grid }) => {
             <div
               onClick={changeState}
               key={colIndex}
+              onMouseDown={mouseDown}
+              onMouseUp={mouseUp}
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
               style={
                 col.isStart === true
                   ? { backgroundColor: "green" }
