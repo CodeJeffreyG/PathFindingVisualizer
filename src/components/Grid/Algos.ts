e558dc41a26654c3d1bd3c45608f7b26e7f42088
reactdevelopmentlpcc@gmail.com <jeffrey.graham600@gmail.com>
2023-03-29 14:47:29 -0400

    a bit closer

--- src/components/Grid/Algos.ts
+++ src/components/Grid/Algos.ts
@@ -16,32 +16,34 @@ const Dfs = (
 
   while (stack.length !== 0) {
     let currentNode = stack.pop();
-    if (!currentNode) continue;
+    if (!currentNode) return;
     if (currentNode.isFinish) return;
     currentNode.isVisited = true;
 
     let tempGrid = [...grid];
     setGrid(tempGrid);
 
-    for (let i = 0; i <= 1; i += 1)
-      for (let j = 0; j <= 1; j += 1)
+    for (let i = 0; i < 2; i += 1) {
+      for (let j = 0; j < 2; j += 1) {
         if (check(currentNode.row + i, currentNode.col + j, grid)) {
           stack.push(grid[currentNode.row + i][currentNode.col + j]);
         }
+      }
+    }
+
     console.log(currentNode);
   }
 };
 
 const check = (row: number, col: number, grid: Array<Array<Node>>): boolean => {
-  let inbounds = row >= 0 && row <= 8 && col >= 0 && col < 25;
+  if (grid[row][col] === undefined) return false;
+  const inbounds = row >= 0 && row <= 7 && col >= 0 && col <= 25;
+  const correctCriteria =
+    !grid[row][col].isWall &&
+    !grid[row][col].isStart &&
+    !grid[row][col].isVisited;
 
-  if (inbounds)
-    if (
-      !grid[row][col].isWall &&
-      !grid[row][col].isStart &&
-      !grid[row][col].isVisited
-    )
-      return true;
+  if (inbounds && correctCriteria) return true;
 
   return false;
 };
